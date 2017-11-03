3a362d31eb2157bc069dd1fd6a19062081576f1b
Brandon <brandonburgoafigueroa@gmail.com>
2017-11-02 20:19:58 -0400

    Near monster

--- lib/Map.rb
+++ lib/Map.rb
@@ -6,46 +6,75 @@ class Map
         @PosOfPerson=[]
         @PosOfMonster=[]
     end
+
     def reset
         @caves=[[]]
         @LastCave=0
         @PosOfPerson=[]
         @PosOfMonster=[]
     end
+
     def CreateCave (posx, posy)
         if (HasCave(posx, posy))
-            @caves[posx][posy]=Cave.new
-            @LastCave=@LastCave+1
-            @caves[posx][posy].AddNumberOfCave(@LastCave)
-            
-            return "Cueva añadida"
-        else
             return "Ya existe una cueva"
+        else
+            AddCave(posx, posy)
+            return "Cueva añadida"    
         end
     end
+
+    def AddCave(posx, posy)
+        @caves[posx][posy]=Cave.new
+        IncrementLastCave()
+        AddNumberOfCave(posx, posy)
+    end
+
+    def IncrementLastCave()
+        @LastCave=@LastCave+1
+    end
+
+    def AddNumberOfCave(posx, posy)
+        @caves[posx][posy].AddNumberOfCave(@LastCave)
+    end
+
     def HasCave(posx, posy)
-        if (@caves[posx][posy]!=nil)
+        if (@caves[posx][posy]==nil)
             return false
         else
             return true
         end
     end
+
     def AssignMonster(posx, posy)
-        if (@caves[posx][posy]!=nil)
-            @PosOfMonster=[posx, posy]
+        if (HasCave(posx, posy))
+            UpdatePosMonster(posx, posy)
             return "Monstruo agregado"
         else
             return "No hay una cueva en esa posicion"
         end
     end
+
+    def UpdatePosMonster(posx, posy)
+        @PosOfMonster=[posx, posy] 
+    end
+
     def AssignAvatar(posx, posy)
-        if (@caves[posx][posy]!=nil)
-            @PosOfPerson=[posx, posy]
-            return "Personaje agregado a la "+ @caves[posx][posy].GetNumber()
+        if (HasCave(posx, posy))
+            UpdatePosPerson(posx, posy)
+            return "Personaje agregado a la "+ NumberOfCaveInPos(posx, posy)
         else
             return "No hay una cueva en esa posicion"
         end
     end
+
+    def NumberOfCaveInPos(posx, posy)
+        return @caves[posx][posy].GetNumber()
+    end
+
+    def UpdatePosPerson(posx, posy)
+        @PosOfPerson=[posx, posy]
+    end
+
     def FeelSmell
         if (NearMonster())
             return "Siento un olor"
@@ -53,6 +82,7 @@ class Map
             return ""
         end
     end
+
     def NearMonster()
         if (@PosOfMonster[0]-@PosOfPerson[0]==1 || @PosOfMonster[1]-@PosOfPerson[1]==1 || @PosOfMonster[0]-@PosOfPerson[0]==-1 || @PosOfMonster[1]-@PosOfPerson[1]==-1)
             return true
